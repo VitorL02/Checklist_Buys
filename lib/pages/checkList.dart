cc11dfa5fe98a885aea737fab9e0addd6171d043
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-05-01 01:25:12 -0300

    Realizando a TODO de imagem da splash art
    Adicionado a images ao pubspec e juntamente finalizando a TODO de inserir uma imagem em splash screen.dart

--- lib/pages/checkList.dart
+++ lib/pages/checkList.dart
@@ -6,7 +6,7 @@ class CheckListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+     color:Colors.white,
     );
   }
 }
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -2,16 +2,27 @@ import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:checklist_buys/pages/checkList.dart';
 import 'package:flutter/material.dart';
 
+
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
+  const SplashScreen({ Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
+  
     return AnimatedSplashScreen(
       nextScreen: const CheckListPage(),
-      splash:'lib/assets/car_splashArt.png',
+      splash: 'lib/assets/car_splashArt.png',
+      backgroundColor: Colors.black,
+      duration:3000,
+      centered: true,
+      splashTransition: SplashTransition.slideTransition,
+ 
       //TODO adicionar a imagem do carrinho e a cor da pagina principal que sera preta
       //TODO adicionar uma cor de transição (provavelmente Rosa)
     );
   }
+}
+
+class PageTransitionType {
 }
