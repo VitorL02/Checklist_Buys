4f52a4799a5f6a7446673a4ac96cb7e6aacad1fa
Vitor Lucas <vitorcrispim@yahoo.com.br>
2022-05-02 05:57:30 -0300

    Criando o modal de adiconar uma nova compra
    Criando o botão de adicionar uma nova tarefa com o showDialog

--- lib/pages/checkList.dart
+++ lib/pages/checkList.dart
@@ -1,5 +1,5 @@
 // ignore: file_names
-// ignore_for_file: file_names, duplicate_ignore, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: file_names, duplicate_ignore, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -9,54 +9,107 @@ class CheckListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'CheckList de Compras',
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            Divider(),
-            SizedBox(height: 20),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: Container(
-                    height: 30,
-                    width: 30,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        shape: BoxShape.circle),
-                    child:Icon(Icons.check)
-                  ),
-                  title: Text(
-                    'Titulo da Compra',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey[200]),
-                  ),
-                );
-              },
-            ),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'CheckList de Compras',
+                style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Divider(),
+              SizedBox(height: 20),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            shape: BoxShape.circle),
+                        child: Icon(Icons.check, color: Colors.white)),
+                    title: Text(
+                      'Titulo da Compra',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey[200]),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    floatingActionButton: FloatingActionButton(
-      onPressed:() {},
-      child:Icon(Icons.add),
-      backgroundColor:Theme.of(context).primaryColor
-    ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => SimpleDialog(
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+                backgroundColor: Colors.grey[800],
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                title: Row(
+                  children: [
+                    Text(
+                      "Nova Compra",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Spacer(),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.cancel, color: Colors.red))
+                  ],
+                ),
+                children: [
+                  Divider(),
+                  TextFormField(
+                    style: TextStyle(
+                        fontSize: 18, color: Colors.white, height: 1.5),
+                    autofocus: true,
+                    decoration: InputDecoration(
+                      hintText: 'Exemplo: Computador Novo',
+                      hintStyle: TextStyle(color: Colors.white70),
+                    ),
+                  ),
+                  SizedBox(height: 70),
+                  // ignore: deprecated_member_use
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    // ignore: deprecated_member_use
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      onPressed: () {},
+                      child: Text('Adicionar'),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Theme.of(context).primaryColor),
     );
   }
 }
